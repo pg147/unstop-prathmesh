1f2014d9b32bbb04414123678233553f2dd6f5ae
pg147 <xyzprathamesh147@gmail.com>
2024-12-21 19:04:07 +0530

    React Router installed and added in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+// React Router DOM
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+// Components
 import Login from "./pages/login/Login";
+import Mainpage from "./pages/main/Mainpage";
 
 export default function App() {
   return (
-    <div className="font-poppins">
-      <Login />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/auth/login" element={<Login />} />
+        <Route path="/" element={<Mainpage />} />
+      </Routes>
+    </Router>
   )
 }
